19b0a29c65b697bf936150213395c43db439dcae
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2019-10-15 19:21:05 -0500

    Clean up some formatting stuff so things line up

--- contentScript.js
+++ contentScript.js
@@ -14,25 +14,33 @@ function removeProjections () {
         if (    typeof teamNameOne != "undefined"
         ) {
             clearInterval (timer);
+            // fantasy cast projection
             var projections = document.getElementsByClassName("jsx-339185608");
             for (var i = projections.length - 1; i >= 0; i--) {
               projections[i].parentElement.removeChild(projections[i]);
             }
 
+            // fantasy cast probability banner
             var probabilities = document.getElementsByClassName("winProbTracker");
             for (var i = probabilities.length - 1; i >= 0; i--) {
               probabilities[i].parentElement.removeChild(probabilities[i]);
             }
 
+            // projection header
             var projHeaders = document.querySelectorAll('[title="ESPN Projection"]');
             for (var i = projHeaders.length - 1; i >= 0; i--) {
-              projHeaders[i].parentElement.removeChild(projHeaders[i]);
+              projHeaders[i].innerText = "Noproj";
             }
 
+            // individual player projections
             var rows = document.getElementsByClassName("Table2__tr Table2__tr--lg Table2__odd");
             for (var i = rows.length - 1; i >= 0; i--) {
-              var projValue = rows[i].cells[5];
-              projValue.parentElement.removeChild(projValue);
+              if (rows[i].cells[0].getAttribute('class').includes('total-col')) {
+                rows[i].cells[4].querySelectorAll('div')[0].innerText = "--";
+              } else {
+                var projValue = rows[i].cells[5];
+                projValue.querySelectorAll('span')[0].innerText = "--";
+              }
             }
 
         }
